58902b2f34e969cf4911dd8693640041061b3f3c
Manuel Heilmeier <m.heilmeier@tiffinger-thiel.de>
2023-11-21 20:55:25 +0100

    Fun with triangles

--- main2.js
+++ main2.js
@@ -9,17 +9,27 @@ var two = new Two(params).appendTo(elem);
 
 const GRID_SIZE = 40;
 
+// Draw grid
+const drawTriangle = (x, y, size = 10, fill='hsla(200, 100%, 50%, 0.5)') => {
+  var triangle = two.makePolygon(x, y, size, 3);
+  triangle.linewidth = 0; 
+  triangle.fill = fill;
+  triangle.rotation = Math.PI * 2 / Math.random() * 8
+}
 
 // Draw grid
 const drawGrid = (size=GRID_SIZE) => {
   for(let x = 0; x < two.width; x+= size) {
     for(let y = 0; y < two.width; y+= size) {
-      two.makeLine(10, 20 ,30 ,40)
+      //two.makeLine(10, 20 ,30 ,40)
+      drawTriangle(x*Math.random() , y*Math.random(), Math.random()*20)
+
     }
   }
 }
 
-drawGrid()
+drawGrid(30)
+
 
 // Build stuff
 two.update();
